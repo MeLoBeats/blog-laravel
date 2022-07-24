d89279289d7d659c8948d2dae02dbe4ff8f3aec6
MeLoBeats <lamelobeats@gmail.com>
2022-07-24 15:40:16 +0200

    Seeding database and tables

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,9 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
 use App\Models\Category;
+use App\Models\Comment;
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -18,15 +20,16 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         $categories = Category::factory(10)->create();
-        \App\Models\User::factory()
-            ->count(15)
-            ->create()
-            ->each(function ($user) use ($categories) {
-                Post::factory(rand(1, 4))->create([
-                    'user_id' => $user->id,
-                    'category_id' => ($categories->random(1)->first())->id
-                ]);
-            });
+        $users = User::factory(15)->create();
+        Post::factory(20)->create([
+            'user_id' => ($users->random(1)->first())->id,
+            'category_id' => ($categories->random(1)->first())->id
+        ])->each(function ($post) use ($users) {
+            Comment::factory(rand(0, 2))->create([
+                "post_id" => $post->id,
+                'user_id' => ($users->random(1)->first())->id,
+            ]);
+        });
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
